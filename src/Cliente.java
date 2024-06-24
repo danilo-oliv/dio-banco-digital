f2c0d71db7f958a730ca8ae9c0ca6dd9d3183b37
danilo <daniloborges@unipam.edu.br>
2024-06-24 10:18:54 -0300

    Abstração

--- src/Cliente.java
+++ src/Cliente.java
@@ -2,6 +2,18 @@
 public class Cliente {
 
 	private String nome;
+	private String cpf;
+	private String endereco;
+
+	public Cliente(String nome, String cpf, String endereco) {
+		this.nome = nome;
+		this.cpf = cpf;
+		this.endereco = endereco;
+	}
+
+	public String getCpf() { return cpf; }
+
+	public String getEndereco() { return endereco; }
 
 	public String getNome() {
 		return nome;
